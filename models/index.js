2f5ab564f661a5469e9ff8237e892590e04ca012
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-25 21:26:34 -0400

    added associations between User and Search. Exported the Models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Search = require('./Search');
 
-module.exports = { User };
+User.hasMany(Search, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Search.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Search };

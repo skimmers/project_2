31929f0be7fc5edaa10f7798081e7e24824ecaab
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-28 17:09:31 -0400

    added trailRoutes route for /:id - keep getting a 500 status code error. Cant find api/trails/:id for some reason

--- controllers/api/trailRoutes.js
+++ controllers/api/trailRoutes.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
 const { User, Search, Comment } = require('../../models');
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const trailData = await Search.findOne();
+        const trailData = await Search.findByPk(req.params.id, {
+            include: {
+                model: Comment,
+                model: User,
+                exclude: {
+                    attributes: ['password']
+                },
+            },
+        });
 
+        console.log(trailData);
+        const getTrail = trailData.map(trail => trail.get({ plain: true }));
         res.status(400).json(trailData);
+
+        res.render('results', getTrail);
+
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
 
-
+module.exports = router;

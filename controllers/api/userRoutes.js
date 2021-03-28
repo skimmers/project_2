836a59cb8a95b8edf4514b184629a77e96c2b3f9
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-28 18:48:08 -0400

    fixed signup page route bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -96,15 +96,13 @@ router.post('/logout', (req, res) => {
 // POST route for signup page
 router.post('/signup', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    const validPassword = await userData.checkPassword(req.body.password);
+    const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.status(200).json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {

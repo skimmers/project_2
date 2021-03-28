8744a3ff9d03cbf163407ceb2a639327c0b0f817
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-28 15:30:59 -0400

    created routes for finding trail information

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
-const { Project, User } = require("../models");
+const { Comment, User, Search } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
+//GET route to handle homepage 
+router.get("/", withAuth, async (req, res) => {
   try {
+
     res.render("homepage", {
       logged_in: req.session.logged_in,
     });
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// Use withAuth middleware to prevent access to route
+// GET route to handle profile page
+// Use withAuth middleware to prevent unauthorized access to route
 router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
@@ -31,35 +35,72 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+// GET route to handle login page
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect("/profile");
-    return;
-  }
+  try {
 
-  res.render("login");
+    if (req.session.logged_in) {
+      res.redirect("/profile");
+      return;
+    }
+  
+    res.render("login");
+
+  } catch (err) {
+
+    res.status(500).json(err);
+
+  }
 });
 
+// GET route to handle signup page
 router.get("/signup", (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect("/profile");
-    return;
+  try {
+
+    if (req.session.logged_in) {
+      res.redirect("/profile");
+      return;
+    }
+  
+    res.render("signup");
+
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.render("signup");
+  
 });
 
-router.get("/results", (req, res) => {
+// GET route to handle results page
+router.get("/results", withAuth, async (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect("/profile");
-  //   return;
-  // }
+  try {
 
-  res.render("results");
+    const searchData = await Search.findOne({
+      where: {
+        trail_name: req.body.trail_name
+      },
+      include: {
+        model: Comment,
+        model: User,
+        exclude: {
+          attributes: ['password']
+        },
+      },
+    }); 
+
+    console.log(searchData);
+    const resultData = searchData.map(result => result.get({ plain: true }));
+    res.status(400).json(resultData);
+
+    res.render('results', resultData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// router.get("/comment")
 
 module.exports = router;

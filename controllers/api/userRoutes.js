bf820bf41c8923efefbd94cf073931cae1107fcd
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-28 15:30:37 -0400

    added routes for creating new comment, getting existing comment

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,21 +1,55 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Comment, Search } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+// *********** Need get route for user profile page
+
+// POST route to handle creating a new comment
+router.post('/comment', withAuth, async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+    const createComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id
     });
+
+    console.log(createComment);
+    
+    const commentData = createComment.map(comment => comment.get({ plain: true }));
+    res.json(commentData);
+
+    res.render('results', commentData);
+
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
+  }
+})
+
+// GET route for a specific user thread page
+router.get('/comment/:id', withAuth, async (req, res) => {
+  try {
+      const getComment = await Comment.findByPk(req.params.id, {
+          include: {
+              model: Search,
+              model: User,
+              exclude: {
+                  attributes: ['password']
+              },
+          },
+      });
+      console.log(getComment);
+  
+      const commentInfo = getComment.map(comment => comment.get({ plain: true }));
+      res.json(commentInfo);
+  
+      res.render('comment', commentInfo);
+
+  } catch (err) {
+      res.status(500).json(err);
   }
 });
 
+
+// POST route for login page
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -48,6 +82,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// POST route for logout page
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -58,6 +93,7 @@ router.post('/logout', (req, res) => {
   }
 });
 
+// POST route for signup page
 router.post('/signup', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });

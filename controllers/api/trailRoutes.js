8b18f7ebd031a2e9a08768d6caef89975fb10873
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-28 21:30:40 -0400

    finished route handling/event handling/initial view

--- controllers/api/trailRoutes.js
+++ controllers/api/trailRoutes.js
@@ -18,34 +18,11 @@ router.get('/', async (req, res) => {
         const allTrails = trailData.map(trail => trail.get({ plain: true }));
         res.status(200).json(allTrails);
 
-        // res.render('results', getTrail);
+        res.render('results', allTrails);
 
     } catch (err) {
         res.status(500).json(err);
     }
 });
-
-router.get('/:id', async (req, res) => {
-    try {
-        const trailData = await Search.findByPk(req.params.id, {
-            include: [
-                { model: Comment },
-                { model: User,
-                    exclude: {
-                        attributes: ['password']
-                    },
-                },
-            ],
-        });
-
-        res.status(200).json(trailData);
-
-        // res.render('results', getTrail);
-
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 
 module.exports = router;
